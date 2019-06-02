40b901b7bc33562eab3064b01b9b37baba3e4324
rfcq <rhuanfelipe@outlook.com>
2019-06-02 16:15:19 -0300

    Add - Categoria e Grupo

--- src/categoria/model/ItemCategoria.java
+++ src/categoria/model/ItemCategoria.java
@@ -1,5 +1,15 @@
 package categoria.model;
 
 public class ItemCategoria {
+	
+	String procedimento;
 
+	public String getAcao() {
+		return procedimento;
+	}
+
+	public void setComponente(String procedimento) {
+		this.procedimento = procedimento;
+	}
+	
 }
---
+++ src/categoria/service/CategoriaServiceImpl.java
@@ -0,0 +1,34 @@
+package categoria.service;
+
+import java.util.List;
+
+import categoria.model.Categoria;
+import servicoGenerico.ServicoGenerico;
+
+public class CategoriaServiceImpl implements ServicoGenerico<Categoria, Integer>{
+
+	@Override
+	public List<Categoria> listar() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void salvar(Categoria entidade) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void remover(Categoria entidade) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void fazerAniversario() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
